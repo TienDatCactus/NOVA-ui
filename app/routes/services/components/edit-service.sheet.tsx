12f73718bb181789af47f61eaa72bc7b2b6d2a31
TienDat <multivncraft@gmail.com>
2025-11-03 10:45:11 +0700

    hot fix

--- app/routes/services/components/edit-service.sheet.tsx
+++ app/routes/services/components/edit-service.sheet.tsx
@@ -212,7 +212,6 @@ export default function EditServiceSheet({
                     <FormControl>
                       <Input
                         type="number"
-                        max={9999999999}
                         onInput={handleLimitInput}
                         placeholder="0"
                         {...field}
